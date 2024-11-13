4372c6390dcc3b474a1aac3895d9aa27a7e8a695
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2024-11-13 11:28:42 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AppRoutes from './utils/AppRoutes'
-import Footer from './components/footer'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppRoutes from './utils/AppRoutes';
+import Footer from './components/footer';
 
 function App() {
-  const router = createBrowserRouter(AppRoutes)
-  return<>
-  <RouterProvider router={router}/>
-  <Footer/>
-  </>
+
+  const router = createBrowserRouter(AppRoutes);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
